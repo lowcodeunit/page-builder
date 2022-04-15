933e1e9fdce56c028e42a0d5122d3af996f9086d
mcgear <michael.gearhardt@gmail.com>
2022-04-15 16:01:21 -0600

    Try with this config

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,25 @@
 const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = function override(config, env) {
-    if (!config.plugins) {
-        config.plugins = [];
-    }
+  if (!config.plugins) {
+    config.plugins = [];
+  }
 
-    config.plugins.push(
-      new ModuleFederationPlugin({
-        name: 'lcu-page-builder',
-        library: { type: 'var', name: 'lcu-page-builder' },
-        filename: 'remoteEntry.js',
-        // runtime: string | false,
-        remotes: [],
-        exposes: [],
-        shared: ['grapesjs', 'react', 'react-dom'],
-      }),
-    );
+  config.plugins.push(
+    new ModuleFederationPlugin({
+      name: 'lcu-page-builder',
+      library: { type: 'var', name: 'lcu-page-builder' },
+      filename: 'remoteEntry.js',
+      // runtime: string | false,
+      remotes: [],
+      exposes: [],
+      shared: {
+        grapesjs: { singleton: true },
+        react: { singleton: true, eager: true }, //, requiredVersion: packageJsonDeps.react },
+        'react-dom': { singleton: true, eager: true }, //, requiredVersion: packageJsonDeps["react-dom"] }
+      },
+    })
+  );
 
-    return config;
-}
+  return config;
+};
