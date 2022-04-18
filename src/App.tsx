df7895be4f04d02b61412307c1b74f9f7934e9f5
mcgear <michael.gearhardt@gmail.com>
2022-04-18 16:46:04 -0600

    Adjsuted id for main component

--- src/App.tsx
+++ src/App.tsx
@@ -20,40 +20,51 @@ class App extends React.Component {
     let appLookup =
       queries.appLookup || this.winAny.LCU?.State?.ApplicationLookup;
 
-    let appFilesResp = await fetch(`/api/lowcodeunit/download/${appLookup}/files/content`);
+    let appFilesResp = await fetch(
+      `/api/lowcodeunit/download/${appLookup}/files/content`
+    );
 
     let appFiles = await appFilesResp.json();
 
-    debugger;
-    grapesjs.init({
-      container: '#gjs',
-      components: appFiles.Model['index.html'] || '', 
+    let grapesInit = {
+      components: appFiles.Model['index.html'] || '',
       style: appFiles.Model['css/style.css'] || '',
       plugins: [
         (editor) =>
           webpage(editor, {
             exportOpts: false,
+            ...(this.winAny?.LCU?.State?.webpage || {}),
           }),
         (editor) =>
           exporter(editor, {
             btnLabel: 'Export to Fathym',
             sendToUrl: `/api/lowcodeunit/deploy/${appLookup}/zip`,
+            ...(this.winAny?.LCU?.State?.exporter || {}),
           }),
       ],
       storageManager: {
         id: `gjs-${appLookup}`, // Prefix identifier that will be used on parameters
-        type: 'local',          // Type of the storage
-        autosave: true,         // Store data automatically
-        autoload: true,         // Autoload stored data on init
-        stepsBeforeSave: 1,     // If autosave enabled, indicates how many changes are necessary before store method is triggered
+        type: 'local', // Type of the storage
+        autosave: true, // Store data automatically
+        autoload: true, // Autoload stored data on init
+        stepsBeforeSave: 1, // If autosave enabled, indicates how many changes are necessary before store method is triggered
       },
-    });
+      ...(this.winAny?.LCU?.State || {}),
+      container: `#gjs-${appLookup}`,
+    };
+
+    grapesjs.init(grapesInit);
   }
 
   public render() {
+    let queries = queryString.parse(window.location.search);
+
+    let appLookup =
+      queries.appLookup || this.winAny.LCU?.State?.ApplicationLookup;
+
     return (
       <div>
-        <div id="gjs"></div>
+        <div id={`gjs-${appLookup}`}></div>
       </div>
     );
   }
